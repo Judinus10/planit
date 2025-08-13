91316c97c531ca4382a5d5e1183798342cdb021e
Judinus10 <judes.20222403@iit.ac.lk>
2025-08-13 16:32:17 +0530

    removed unwanted codes

--- send_email.php
+++ send_email.php
@@ -1,39 +1,4 @@
 <?php
-// use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\Exception;
-
-// require 'vendor/autoload.php';  // Include Composer's autoloader
-
-// $otp = rand(100000, 999999);
-// $user_email = 'jjudinas@gmail.com';  // Replace with recipient email
-
-// $mail = new PHPMailer(true);
-
-// try {
-//     //Server settings
-//     $mail->isSMTP();
-//     $mail->Host       = 'smtp.gmail.com';
-//     $mail->SMTPAuth   = true;
-//     $mail->Username   = 'judinushu@gmail.com';   // Your Gmail address
-//     $mail->Password   = 'tlcm hmuq lvhv fynw';      // Your Gmail App Password
-//     $mail->SMTPSecure = 'tls';
-//     $mail->Port       = 587;
-
-//     //Recipients
-//     $mail->setFrom('judinushu@gmail.com', 'Xampp Project');
-//     $mail->addAddress($user_email);
-
-//     //Content
-//     $mail->isHTML(false);
-//     $mail->Subject = 'Your OTP Code';
-//     $mail->Body    = "Your OTP code is: $otp";
-
-//     $mail->send();
-//     echo 'OTP email sent successfully!';
-// } catch (Exception $e) {
-//     echo "Mailer Error: {$mail->ErrorInfo}";
-// }
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
