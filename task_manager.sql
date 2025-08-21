b4ddd359b2a9046eff9ce7ca30ee7e838959e795
Judinus10 <judes.20222403@iit.ac.lk>
2025-08-21 11:33:56 +0530

    imported the sql for the project

--- task_manager.sql
+++ task_manager.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 19, 2025 at 11:32 AM
+-- Generation Time: Aug 21, 2025 at 07:47 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -101,7 +101,8 @@ CREATE TABLE `projects` (
 --
 
 INSERT INTO `projects` (`id`, `name`, `description`, `created_by`, `created_at`) VALUES
-(1, 'planit', 'a task managing website', 4, '2025-08-19 06:13:48');
+(1, 'planit', 'a task managing website', 4, '2025-08-19 06:13:48'),
+(2, 'nexa', 'a desktop assistant', 7, '2025-08-20 07:50:18');
 
 -- --------------------------------------------------------
 
@@ -122,7 +123,8 @@ CREATE TABLE `project_members` (
 --
 
 INSERT INTO `project_members` (`id`, `user_id`, `project_id`, `role`, `joined_at`) VALUES
-(1, 7, 1, 'member', '2025-08-19 06:38:16');
+(1, 7, 1, 'member', '2025-08-19 06:38:16'),
+(2, 4, 2, 'member', '2025-08-20 07:51:51');
 
 -- --------------------------------------------------------
 
@@ -134,7 +136,7 @@ CREATE TABLE `subtasks` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `task_id` int(11) NOT NULL,
-  `status` enum('To-do','Completed') DEFAULT 'To-do',
+  `status` enum('To-do','completed') DEFAULT 'To-do',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -143,8 +145,9 @@ CREATE TABLE `subtasks` (
 --
 
 INSERT INTO `subtasks` (`id`, `title`, `task_id`, `status`, `created_at`) VALUES
-(1, 'create login page', 4, 'Completed', '2025-08-19 06:28:05'),
-(3, 'create home page', 4, 'To-do', '2025-08-19 06:30:17');
+(7, 'create login page', 4, 'To-do', '2025-08-21 05:07:23'),
+(9, 'create home page', 4, 'To-do', '2025-08-21 05:25:29'),
+(10, 'create edit detais page', 4, 'To-do', '2025-08-21 05:36:53');
 
 -- --------------------------------------------------------
 
@@ -159,7 +162,7 @@ CREATE TABLE `tasks` (
   `project_id` int(11) NOT NULL,
   `assigned_to` int(11) DEFAULT NULL,
   `user_id` int(11) NOT NULL,
-  `status` enum('To-do','On-progress','Completed') DEFAULT 'To-do',
+  `status` enum('To-do','on-progress','completed') DEFAULT 'To-do',
   `priority` enum('Low','Medium','High') DEFAULT 'Medium',
   `due_date` datetime DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
@@ -172,15 +175,14 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`id`, `title`, `description`, `project_id`, `assigned_to`, `user_id`, `status`, `priority`, `due_date`, `created_at`, `updated_at`, `notified`) VALUES
-(1, 'structure', 'create diagrams ', 1, 4, 4, 'Completed', 'Medium', '2025-08-20 00:00:00', '2025-08-19 06:14:59', '2025-08-19 06:25:33', 0),
-(2, 'design', 'design web pages', 1, 7, 4, 'On-progress', 'Medium', '2025-08-19 15:24:00', '2025-08-19 06:15:39', '2025-08-19 09:24:52', 1),
-(3, 'database', 'create all database ', 1, 4, 4, 'To-do', 'High', '2025-08-23 00:00:00', '2025-08-19 06:19:04', '2025-08-19 06:19:04', 0),
-(4, 'frontEnd', 'create FrontEnd for all pages', 1, 4, 4, 'To-do', 'Medium', '2025-08-26 00:00:00', '2025-08-19 06:19:41', '2025-08-19 06:24:57', 0),
-(5, 'backEnd', 'create backEnd ', 1, 7, 4, 'To-do', 'High', '2025-08-30 00:00:00', '2025-08-19 06:20:19', '2025-08-19 06:39:47', 0),
-(6, 'connect ', 'connect fend and bend', 1, 7, 4, 'To-do', 'Medium', '2025-08-31 00:00:00', '2025-08-19 06:21:51', '2025-08-19 06:41:15', 0),
+(1, 'structure', 'create diagrams ', 1, 4, 4, 'completed', 'Medium', '2025-08-20 00:00:00', '2025-08-19 06:14:59', '2025-08-21 03:53:10', 0),
+(2, 'design', 'design web pages', 1, 7, 4, 'completed', 'Medium', '2025-08-19 15:24:00', '2025-08-19 06:15:39', '2025-08-21 03:29:54', 1),
+(3, 'database', 'create all database ', 1, 4, 4, 'completed', 'High', '2025-08-23 00:00:00', '2025-08-19 06:19:04', '2025-08-20 09:45:31', 0),
+(4, 'frontEnd', 'create FrontEnd for all pages', 1, 7, 4, 'on-progress', 'High', '2025-08-26 00:00:00', '2025-08-19 06:19:41', '2025-08-21 05:12:33', 0),
+(5, 'backEnd', 'create backEnd ', 1, 7, 4, 'on-progress', 'High', '2025-08-30 00:00:00', '2025-08-19 06:20:19', '2025-08-21 03:55:03', 0),
+(6, 'connect ', 'connect fend and bend', 1, 7, 4, 'on-progress', 'Medium', '2025-08-31 00:00:00', '2025-08-19 06:21:51', '2025-08-21 03:38:25', 0),
 (7, 'testing and debugging', 'testing and debug projec', 1, 7, 4, 'To-do', 'Medium', '2025-09-04 00:00:00', '2025-08-19 06:22:29', '2025-08-19 07:01:13', 0),
-(8, 'deploy project', 'try to find a hosting platform and deploy project', 1, 7, 4, 'To-do', 'Medium', '2025-09-06 00:00:00', '2025-08-19 06:23:10', '2025-08-19 08:08:01', 0),
-(10, 'gtghneryh', 'etherhe5j', 1, 4, 4, 'To-do', 'Medium', '2025-08-20 13:40:00', '2025-08-19 08:10:35', '2025-08-19 08:18:37', 0);
+(8, 'deploy project', 'try to find a hosting platform and deploy project', 1, 7, 4, 'To-do', 'Medium', '2025-09-06 00:00:00', '2025-08-19 06:23:10', '2025-08-21 04:00:06', 0);
 
 -- --------------------------------------------------------
 
@@ -287,7 +289,7 @@ ALTER TABLE `invitations`
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `password_resets`
@@ -299,19 +301,19 @@ ALTER TABLE `password_resets`
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `project_members`
 --
 ALTER TABLE `project_members`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `subtasks`
 --
 ALTER TABLE `subtasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `tasks`
